2bb550029aacb1f52c618be660dd50864b1a7ae1
Rexford Machu <rexfordmachu@gmail.com>
2021-07-17 09:41:52 +0000

    The lesson on variables in Go has been completed. Go has a beautiful structure.

---
+++ Variables/variable.go
@@ -0,0 +1,20 @@
+package main 
+
+import(
+	"fmt"
+)
+
+func main(){
+	var a, b ,c int = 10, 90, 67
+	fmt.Println(a,b,c)
+
+	var e string = " Go is a beautiful language"
+
+	fmt.Println("The variable e contains : ", e)
+
+	f := "The letter f"
+	fmt.Println(f)
+
+	var g string
+	fmt.Println("variable g should be empty as it is not initialized: ",g)
+}
