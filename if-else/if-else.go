f10bc827f707bcb47895e6bf96076922030301c0
Rexford Machu <rexfordmachu@gmail.com>
2021-07-19 21:13:26 +0000

    There are no ternary if statement in go. Also, the else and else if need to be on the same line else there is an error.

---
+++ if-else/if-else.go
@@ -0,0 +1,22 @@
+package main 
+
+import(
+	"fmt"
+)
+
+func main(){
+	if 7%2 == 0{
+		fmt.Println("Seven is even")
+	}else {
+		fmt.Println("Seven is odd")
+	}
+
+
+	if num:= 9; num <0 {
+		fmt.Println("Number is negative")
+	}	else if num <10 {
+		fmt.Println("Number is 1 digit")
+	}	else {
+		fmt.Println("XXX")
+	}
+}
