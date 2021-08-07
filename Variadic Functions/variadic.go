54cec8a3501e8b1b1bdc2ab7898ae4a06e044dcc
Rexford Machu <rexfordmachu@gmail.com>
2021-08-07 00:16:50 +0000

    Lesrnt variadic functions that are needed

---
+++ Variadic Functions/variadic.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+var total int
+
+
+func addAll(nums ...int) int{
+	for _,num := range nums{
+		total=+ num
+		fmt.Println(total)
+}
+fmt.Println(total)
+
+	return total
+}
+func main(){
+	fmt.Println(addAll(3,4,5,9,3,4,2))
+}
