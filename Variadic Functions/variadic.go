a276ce23a2af852b19a190a0a1deeea29ef2134a
Rexford Machu <rexfordmachu@gmail.com>
2021-08-12 11:44:46 +0000

    Recursion module completed using factorial problem.

--- Variadic Functions/variadic.go
+++ Variadic Functions/variadic.go
@@ -6,16 +6,15 @@ import (
 
 var total int
 
-
-func addAll(nums ...int) int{
-	for _,num := range nums{
-		total=+ num
+func addAll(nums ...int) int {
+	for _, num := range nums {
+		total = +num
 		fmt.Println(total)
-}
-fmt.Println(total)
+	}
+	fmt.Println(total)
 
 	return total
 }
-func main(){
-	fmt.Println(addAll(3,4,5,9,3,4,2))
+func main() {
+	fmt.Println(addAll(3, 4, 5, 9, 3, 4, 2))
 }
